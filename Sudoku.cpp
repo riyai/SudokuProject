1f24f04a5ae05cbf1bc169e28f08fb6628439029
riyai <72334955+riyai@users.noreply.github.com>
2022-04-24 16:26:47 +0530

    Update and rename Sodukuu.cpp to Sudoku.cpp

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -4,12 +4,18 @@
 
 using namespace std;
 
+//Define the size of the grid
+const int SQRTDIM = 3;
+const int DIM = SQRTDIM * SQRTDIM;
+
+//Create a class Sudoku
+//Has functions for all operations
 class Sudoku {
 private:
-  int grid[9][9];
-  int solnGrid[9][9];
-  int guessNum[9];
-  int gridPos[81];
+  int grid[DIM][DIM];
+  int solnGrid[DIM][DIM];
+  int guessNum[DIM];
+  int gridPos[DIM * DIM];
   bool grid_status;
 
 public:
@@ -19,43 +25,27 @@ public:
   void createSeed();
   void printGrid();
   bool solveGrid();
-  string getGrid();
   void countSoln(int &number);
   void genPuzzle();
   bool verifyGridStatus();
+  void getCell(int grow, int gcol);
 };
 
-// START: Get grid as string in row major order
-string Sudoku::getGrid()
-{
-  string s = "";
-  for(int row_num=0; row_num<9; ++row_num)
-  {
-    for(int col_num=0; col_num<9; ++col_num)
-    {
-      s = s + to_string(grid[row_num][col_num]);
-    }
-  }
-
-  return s;
-}
-// END: Get grid as string in row major order
-
-
-// START: Generate random number
+// START: To generate random number
 int genRandNum(int maxLimit)
 {
   return rand()%maxLimit;
 }
-// END: Generate random number
+// END
 
 
-// START: Helper functions for solving grid
-bool FindUnassignedLocation(int grid[9][9], int &row, int &col)
+// START: These are helper functions for solving grid
+//Find unassigned location
+bool FindUnassignedLocation(int grid[DIM][DIM], int &row, int &col)
 {
-    for (row = 0; row < 9; row++)
+    for (row = 0; row < DIM; row++)
     {
-        for (col = 0; col < 9; col++)
+        for (col = 0; col < DIM; col++)
         {
             if (grid[row][col] == UNASSIGNED)
                 return true;
@@ -64,9 +54,10 @@ bool FindUnassignedLocation(int grid[9][9], int &row, int &col)
     return false;
 }
 
-bool UsedInRow(int grid[9][9], int row, int num)
+//Check if a number valid for a row
+bool UsedInRow(int grid[DIM][DIM], int row, int num)
 {
-    for (int col = 0; col < 9; col++)
+    for (int col = 0; col < DIM; col++)
     {
         if (grid[row][col] == num)
             return true;
@@ -75,9 +66,10 @@ bool UsedInRow(int grid[9][9], int row, int num)
     return false;
 }
 
-bool UsedInCol(int grid[9][9], int col, int num)
+//Check if a number valid for a column
+bool UsedInCol(int grid[DIM][DIM], int col, int num)
 {
-    for (int row = 0; row < 9; row++)
+    for (int row = 0; row < DIM; row++)
     {
         if (grid[row][col] == num)
             return true;
@@ -86,11 +78,12 @@ bool UsedInCol(int grid[9][9], int col, int num)
     return false;
 }
 
-bool UsedInBox(int grid[9][9], int boxStartRow, int boxStartCol, int num)
+//Check if a number valid for a sub grid
+bool UsedInBox(int grid[DIM][DIM], int boxStartRow, int boxStartCol, int num)
 {
-    for (int row = 0; row < 3; row++)
+    for (int row = 0; row < SQRTDIM; row++)
     {
-        for (int col = 0; col < 3; col++)
+        for (int col = 0; col < SQRTDIM; col++)
         {
             if (grid[row+boxStartRow][col+boxStartCol] == num)
                 return true;
@@ -100,9 +93,10 @@ bool UsedInBox(int grid[9][9], int boxStartRow, int boxStartCol, int num)
     return false;
 }
 
-bool isSafe(int grid[9][9], int row, int col, int num)
+//Safe to use a number or not
+bool isSafe(int grid[DIM][DIM], int row, int col, int num)
 {
-    return !UsedInRow(grid, row, num) && !UsedInCol(grid, col, num) && !UsedInBox(grid, row - row%3 , col - col%3, num);
+    return !UsedInRow(grid, row, num) && !UsedInCol(grid, col, num) && !UsedInBox(grid, row - row%SQRTDIM , col - col%SQRTDIM, num);
 }
 // END: Helper functions for solving grid
 
@@ -110,13 +104,13 @@ bool isSafe(int grid[9][9], int row, int col, int num)
 // START: Create seed grid
 void Sudoku::fillEmptyDiagonalBox(int idx)
 {
-  int start = idx*3;
-  random_shuffle(this->guessNum, (this->guessNum) + 9, genRandNum);
-  for (int i = 0; i < 3; ++i)
+  int start = idx*SQRTDIM;
+  random_shuffle(this->guessNum, (this->guessNum) + DIM, genRandNum);
+  for (int i = 0; i < SQRTDIM; ++i)
   {
-    for (int j = 0; j < 3; ++j)
+    for (int j = 0; j < SQRTDIM; ++j)
     {
-      this->grid[start+i][start+j] = guessNum[i*3+j];
+      this->grid[start+i][start+j] = guessNum[i*SQRTDIM+j];
     }
   }
 }
@@ -128,27 +122,27 @@ void Sudoku::createSeed()
       . | x | .
       . | . | x
   */
-  this->fillEmptyDiagonalBox(0);
-  this->fillEmptyDiagonalBox(1);
-  this->fillEmptyDiagonalBox(2);
+  for (int i = 0; i < SQRTDIM; ++i)
+	this->fillEmptyDiagonalBox(i);
 
   /* Fill the remaining blocks:
       x | x | x
       x | x | x
       x | x | x
   */
-  this->solveGrid(); // TODO: not truly random, but still good enough because we generate random diagonals.
+  if (!this->solveGrid()) // TODO: not truly random, but still good enough because we generate random diagonals.
+	this->createSeed();   // No solution possible so run again
 
   // Saving the solution grid
-  for(int i=0;i<9;i++)
+  for(int i=0;i<DIM;i++)
   {
-    for(int j=0;j<9;j++)
+    for(int j=0;j<DIM;j++)
     {
       this->solnGrid[i][j] = this->grid[i][j];
     }
   }
 }
-// END: Create seed grid
+// END
 
 
 // START: Intialising
@@ -156,25 +150,25 @@ Sudoku::Sudoku()
 {
 
   // Randomly shuffling the array of removing grid positions
-  for(int i=0;i<81;i++)
+  for(int i=0;i<DIM * DIM;i++)
   {
     this->gridPos[i] = i;
   }
 
-  random_shuffle(this->gridPos, (this->gridPos) + 81, genRandNum);
+  random_shuffle(this->gridPos, (this->gridPos) + DIM * DIM, genRandNum);
 
   // Randomly shuffling the guessing number array
-  for(int i=0;i<9;i++)
+  for(int i=0;i<DIM;i++)
   {
     this->guessNum[i]=i+1;
   }
 
-  random_shuffle(this->guessNum, (this->guessNum) + 9, genRandNum);
+  random_shuffle(this->guessNum, (this->guessNum) + DIM, genRandNum);
 
   // Initialising the grid
-  for(int i=0;i<9;i++)
+  for(int i=0;i<DIM;i++)
   {
-    for(int j=0;j<9;j++)
+    for(int j=0;j<DIM;j++)
     {
       this->grid[i][j]=0;
     }
@@ -182,36 +176,36 @@ Sudoku::Sudoku()
 
   grid_status = true;
 }
-// END: Initialising
+// END
 
 // START: Custom Initialising with grid passed as argument
 Sudoku::Sudoku(string grid_str, bool row_major)
 {
-  if(grid_str.length() != 81)
+  if(grid_str.length() != DIM * DIM)
   {
     grid_status=false;
     return;
   }
 
   // First pass: Check if all cells are valid
-  for(int i=0; i<81; ++i)
+  for(int i=0; i<DIM * DIM; ++i)
   {
     int curr_num = grid_str[i]-'0';
-    if(!((curr_num == UNASSIGNED) || (curr_num > 0 && curr_num < 10)))
+    if(!((curr_num == UNASSIGNED) || (curr_num > 0 && curr_num <= DIM)))
     {
       grid_status=false;
       return;
     }
 
-    if(row_major) grid[i/9][i%9] = curr_num;
-    else          grid[i%9][i/9] = curr_num;
+    if(row_major) grid[i/DIM][i%DIM] = curr_num;
+    else          grid[i%DIM][i/DIM] = curr_num;
   }
 
   // Second pass: Check if all columns are valid
-  for (int col_num=0; col_num<9; ++col_num)
+  for (int col_num=0; col_num<DIM; ++col_num)
   {
-    bool nums[10]={false};
-    for (int row_num=0; row_num<9; ++row_num)
+    bool nums[DIM + 1]={false};
+    for (int row_num=0; row_num<DIM; ++row_num)
     {
       int curr_num = grid[row_num][col_num];
       if(curr_num!=UNASSIGNED && nums[curr_num]==true)
@@ -224,10 +218,10 @@ Sudoku::Sudoku(string grid_str, bool row_major)
   }
 
   // Third pass: Check if all rows are valid
-  for (int row_num=0; row_num<9; ++row_num)
+  for (int row_num=0; row_num<DIM; ++row_num)
   {
-    bool nums[10]={false};
-    for (int col_num=0; col_num<9; ++col_num)
+    bool nums[DIM + 1]={false};
+    for (int col_num=0; col_num<DIM; ++col_num)
     {
       int curr_num = grid[row_num][col_num];
       if(curr_num!=UNASSIGNED && nums[curr_num]==true)
@@ -240,12 +234,12 @@ Sudoku::Sudoku(string grid_str, bool row_major)
   }
 
   // Fourth pass: Check if all blocks are valid
-  for (int block_num=0; block_num<9; ++block_num)
+  for (int block_num=0; block_num<DIM; ++block_num)
   {
-    bool nums[10]={false};
-    for (int cell_num=0; cell_num<9; ++cell_num)
+    bool nums[DIM + 1]={false};
+    for (int cell_num=0; cell_num<DIM; ++cell_num)
     {
-      int curr_num = grid[((int)(block_num/3))*3 + (cell_num/3)][((int)(block_num%3))*3 + (cell_num%3)];
+      int curr_num = grid[((int)(block_num/SQRTDIM))*SQRTDIM + (cell_num/SQRTDIM)][((int)(block_num%SQRTDIM))*SQRTDIM + (cell_num%SQRTDIM)];
       if(curr_num!=UNASSIGNED && nums[curr_num]==true)
       {
         grid_status=false;
@@ -256,16 +250,16 @@ Sudoku::Sudoku(string grid_str, bool row_major)
   }
 
   // Randomly shuffling the guessing number array
-  for(int i=0;i<9;i++)
+  for(int i=0;i<DIM;i++)
   {
     this->guessNum[i]=i+1;
   }
 
-  random_shuffle(this->guessNum, (this->guessNum) + 9, genRandNum);
+  random_shuffle(this->guessNum, (this->guessNum) + DIM, genRandNum);
 
   grid_status = true;
 }
-// END: Custom Initialising
+// END
 
 
 // START: Verification status of the custom grid passed
@@ -273,27 +267,27 @@ bool Sudoku::verifyGridStatus()
 {
   return grid_status;
 }
-// END: Verification of the custom grid passed
+// END
 
 
 // START: Printing the grid
 void Sudoku::printGrid()
 {
-  for (int row = 0; row < 9; row++){
-      for (int col = 0; col < 9; col++){
-         if(col == 3 || col == 6)
+  for (int row = 0; row < DIM; row++){
+      for (int col = 0; col < DIM; col++){
+         if(col % SQRTDIM == 0 && col != 0)
             cout << " | ";
          cout << grid[row][col] <<" ";
       }
-      if(row == 2 || row == 5){
+      if(row != 0 && (row + 1) % SQRTDIM == 0){
          cout << endl;
-         for(int i = 0; i<9; i++)
+         for(int i = 0; i<DIM; i++)
             cout << "---";
       }
       cout << endl;
    }
 }
-// END: Printing the grid
+// END
 
 
 // START: Modified Sudoku solver
@@ -305,8 +299,8 @@ bool Sudoku::solveGrid()
     if (!FindUnassignedLocation(this->grid, row, col))
        return true; // success!
 
-    // Consider digits 1 to 9
-    for (int num = 0; num < 9; num++)
+    // Consider digits 1 to DIM
+    for (int num = 0; num < DIM; num++)
     {
         // if looks promising
         if (isSafe(this->grid, row, col, this->guessNum[num]))
@@ -326,7 +320,7 @@ bool Sudoku::solveGrid()
     return false; // this triggers backtracking
 
 }
-// END: Modified Sudoku Solver
+// END
 
 
 // START: Check if the grid is uniquely solvable
@@ -341,7 +335,7 @@ void Sudoku::countSoln(int &number)
   }
 
 
-  for(int i=0;i<9 && number<2;i++)
+  for(int i=0;i<DIM && number<2;i++)
   {
       if( isSafe(this->grid, row, col, this->guessNum[i]) )
       {
@@ -353,16 +347,16 @@ void Sudoku::countSoln(int &number)
   }
 
 }
-// END: Check if the grid is uniquely solvable
+// END
 
 
 // START: Generate puzzle
 void Sudoku::genPuzzle()
 {
-  for(int i=0;i<81;i++)
+  for(int i=0;i<DIM * DIM;i++)
   {
-    int x = (this->gridPos[i])/9;
-    int y = (this->gridPos[i])%9;
+    int x = (this->gridPos[i])/DIM;
+    int y = (this->gridPos[i])%DIM;
     int temp = this->grid[x][y];
     this->grid[x][y] = UNASSIGNED;
 
@@ -375,15 +369,30 @@ void Sudoku::genPuzzle()
     }
   }
 }
-// END: Generate puzzle
+// END
 
+void Sudoku::getCell(int grow, int gcol)
+{
+      int grid1[DIM][DIM];
+    
+      for(int i=0; i<DIM; i++){
+          for(int j=0; j<DIM; j++){
+              grid1[i][j]=grid[i][j];
+          }
+      }
+      solveGrid();
+      grid1[grow][gcol]=grid[grow][gcol];
+      for(int i=0; i<DIM; i++){
+          for(int j=0; j<DIM; j++){
+              grid[i][j]=grid1[i][j];
+          }
+      }
+      printGrid();
+}
 
 // START: The main function
 int main(int argc, char const *argv[])
 {
-  // Initialising seed for random number generation
-  srand(time(NULL));
-
   // Creating an instance of Sudoku
   Sudoku *puzzle = new Sudoku();
 
@@ -397,12 +406,43 @@ int main(int argc, char const *argv[])
 
   // testing by printing the grid
   puzzle->printGrid();
+  
+start:
+  cout << "1. Display complete solution"<< endl<< "2. View specific cell"<<endl;
+    int ch;
+    cin >> ch;
+    if(ch==1){
+        puzzle->solveGrid();
+        puzzle->printGrid();
+    }
+    else if(ch==2){
+        char choice='Y';
+        do{ 
+            int r, c;
+            begin:
+            cout << endl << "Enter the row and coloumn(0 -"<<DIM-1<< ")" << endl;
+            cin >> r >> c;
 
-  cout << endl << "Solution for the given grid:" << endl;
-  puzzle->solveGrid();
+            if((r>=0&&r<DIM)&&(c>=0&&c<DIM)){
+                
+                cout << endl << "Solution for the given cell:" << endl;
+                puzzle->getCell(r, c);
+            }
+            else{
+                cout << "Given values exceed the size limits, enter valid dimensions" << endl;
+                goto begin;
+            }
+        
+            cout << "Continue?(Enter Y for yes/ N for no)";
+            cin >> choice;
 
-  puzzle->printGrid();
-
+        }while(choice!='N'&&choice!='n');
+      } 
+      else{
+          cout << "Not a valid choice" << endl;
+          goto start;
+      }
+  
   return 0;
 }
 // END: The main function
